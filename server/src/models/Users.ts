c541e7ecafab3b4b15e851e58f457e80116dc444
Samuel Santos <107240729+samuel-santos91@users.noreply.github.com>
2024-07-15 00:05:00 +1000

    creates User model

---
+++ server/src/models/Users.ts
@@ -0,0 +1,50 @@
+import { DataTypes, Model, Sequelize } from "sequelize";
+
+const sequelize = new Sequelize(
+  "mysql://your_username:your_password@localhost:3306/your_database"
+);
+
+class User extends Model {
+  public id!: number;
+  public name!: string;
+  public dateOfBirth!: string;
+  public mainLanguage!: string;
+  public secondaryLanguage!: string;
+  public fundingSource!: string;
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    dateOfBirth: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    mainLanguage: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    secondaryLanguage: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    fundingSource: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: "users",
+  }
+);
+
+export default User;
