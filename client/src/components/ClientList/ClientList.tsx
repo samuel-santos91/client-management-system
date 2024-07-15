16e52b3257f7a130b953e3d2dc33f22b7c787280
Samuel Santos <107240729+samuel-santos91@users.noreply.github.com>
2024-07-15 19:05:30 +1000

    organises data display clientlist component

--- client/src/components/ClientList/ClientList.tsx
+++ client/src/components/ClientList/ClientList.tsx
@@ -16,13 +16,11 @@ const ClientList: React.FC = () => {
 
   const fetchData = async () => {
     try {
-      // Set loading state
       const res = await getUsers();
       setUserDataList(res);
     } catch (error) {
       console.error("Error fetching data:", error);
     } finally {
-      // Set loading state to false
     }
   };
 
@@ -31,11 +29,31 @@ const ClientList: React.FC = () => {
   }, []);
 
   return (
-    <div>
-      {userDataList?.map((userData) => (
-        <p key={userData.id}>{userData.name}</p>
-      ))}
-    </div>
+    <main>
+      <h2>Clients List</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Date of Birth</th>
+            <th>Main Language</th>
+            <th>Secondary Language</th>
+            <th>Funding Source</th>
+          </tr>
+        </thead>
+        <tbody>
+          {userDataList?.map((userData) => (
+            <tr key={userData.id}>
+              <td>{userData.name}</td>
+              <td>{userData.dateOfBirth}</td>
+              <td>{userData.mainLanguage}</td>
+              <td>{userData.secondaryLanguage}</td>
+              <td>{userData.fundingSource}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </main>
   );
 };
 
